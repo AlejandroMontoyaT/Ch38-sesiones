391980daa11e43177cba68c1ab4e8af8b50868de
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-02-19 13:28:39 -0600

    CI: se modifico archivo js para agregar nombre en ventana emergente

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log ("hola Ch-38");
+console.log("Hola");
+
+//funcion que envia un saludo en alerta
+function saludarPersona() {
+   alert("hola ch-38");
+   
+  }
+  
+  /**
+ * Pedir el nombre de la persona usando promt()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+function enviarSaludoAPersona(){
+    let nombre = prompt("Ingrese su nombre");
+    alert ('saludarPersona ${message}');
+}   
